d780d2817c99fc8c0a5ac509860199224972f4f3
Saket Choudhary <saketkc@gmail.com>
2015-11-25 10:30:59 -0800

    Init new

--- webserver/moca/analysis.py
+++ webserver/moca/analysis.py
@@ -1,2 +1,27 @@
 import subprocess
 
+class Moca(object):
+    """
+    Main class to analyse motifs
+    """
+
+    def __init__(self):
+        self.
+
+    def perform_mapping(self):
+        raise NotImplementedError
+
+    def run_meme(self):
+        raise NotImplementedError
+
+    def run_fimo(self):
+        raise NotImplementedError
+
+    def call_peaks(self):
+        raise NotImplementedError
+
+    def generate_plots(self):
+        raise NotImplementedError
+
+    def merge_replicates(self):
+        raise NotImplementedError
